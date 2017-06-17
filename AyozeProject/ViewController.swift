67e490a1c4f5f94ac5aece5776f96eda4b9b8e35
Loquat Solutions <stock@loquatsolutions.com>
2017-06-17 12:42:42 +0100

    Initial Commit

---
+++ AyozeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AyozeProject
+//
+//  Created by Loquat Solutions on 17/6/17.
+//  Copyright © 2017 MHP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
